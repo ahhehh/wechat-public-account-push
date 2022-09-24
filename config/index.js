82c9a19838396008a1ed123c3178ed972ff5b79d
ahhehh <114307742+ahhehh@users.noreply.github.com>
2022-09-25 01:48:45 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: '早呀萱宝',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
@@ -65,9 +65,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oa8Ss51NzuCUBt0zlhTiLUei2xo0',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'u3mjp_K5TMubNvTeZM_AdDwArg9A-TmYEN86Lj4o8zI',
       // 所在省份
       province: '广东',
       // 所在城市
